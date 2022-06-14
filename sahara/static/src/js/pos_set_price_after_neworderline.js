7f377c00665c3cbe6b71d98be837e3aa16c9f87a
Montagab Sheha <m.sheha@s-apps.io>
2022-06-14 12:08:44 +0300

    reset price

--- sahara/static/src/js/pos_set_price_after_neworderline.js
+++ sahara/static/src/js/pos_set_price_after_neworderline.js
@@ -3,6 +3,7 @@ odoo.define('sahara.possetpriceafterneworderline', function(require) {
     'use strict';
        const Registries = require('point_of_sale.Registries');
        const ProductScreen = require('point_of_sale.ProductScreen');
+       const NumberBuffer = require('point_of_sale.NumberBuffer');
         const possetpriceafterneworderline = (ProductScreen) =>
            class extends ProductScreen {
                constructor() {
@@ -12,6 +13,7 @@ odoo.define('sahara.possetpriceafterneworderline', function(require) {
                 // console.log('bbbbbbb');
                 self = this;
                 setTimeout(() => {
+                    NumberBuffer.reset();
                     self.state.numpadMode = 'price';
                 }, 200); 
             }
