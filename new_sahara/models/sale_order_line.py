a45f283d01082046362b957a2062464124f35ae6
m.salman <efrfr>
2023-03-30 14:00:07 +0300

    return in report4.4

--- new_sahara/models/sale_order_line.py
+++ new_sahara/models/sale_order_line.py
@@ -20,8 +20,10 @@ class SaleReport(models.Model):
     _inherit = "sale.report"
 
     costq = fields.Float('Cost', readonly=True)
+    margin = fields.Float('Margin')
 
     def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
         fields['costq'] = ",(l.price_total - l.margin) as costq"
+        fields['margin'] = ", SUM(l.margin / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS margin"
         groupby += ', (l.price_total - l.margin)'
         return super(SaleReport, self)._query(with_clause, fields, groupby, from_clause)
