176ef27d942100e91b15a625b908fd1b41cf36e3
saharadev.2022@gmail.com <!Admin123123>
2023-04-03 13:57:28 +0300

    Cost in report 1.2

--- new_sahara/models/repair_order.py
+++ new_sahara/models/repair_order.py
@@ -12,7 +12,7 @@ class StockPicking(models.Model):
 class RepairOrder(models.Model):
     _inherit = 'repair.order'
 
-    repair_cost = fields.Float(string='Repair Cost', compute='_compute_cost', store=True)
+    repair_cost = fields.Float(string='Margin', compute='_compute_cost', store=True)
     inv_total = fields.Float(string='Invoice Total', compute='_compute_inv_total', store=True)
 
     def _compute_cost(self):
