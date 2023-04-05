4ac5a280733a87c68bc106d62b776e00dd67616b
m.salman <efrfr>
2023-04-05 10:16:33 +0300

    pos serial num 1.0

--- new_sahara/models/sale_order.py
+++ new_sahara/models/sale_order.py
@@ -13,14 +13,3 @@ class SaleReport(models.Model):
         groupby += ', (l.price_total - l.margin)'
         return super(SaleReport, self)._query(with_clause, fields, groupby, from_clause)
 
-
-class PosOrderReport(models.Model):
-    _inherit = "report.pos.order"
-    cost_pos = fields.Float('Cost', readonly=True)
-
-    def _select(self):
-        return super(PosOrderReport, self)._select() + \
-               ',(SUM(ROUND((l.qty * l.price_unit) * (100 - l.discount) / 100 / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END, cu.decimal_places)) - SUM(l.price_subtotal - l.total_cost / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END)) AS cost_pos'
-
-    # def _group_by(self):
-    #     return super(PosOrderReport, self)._group_by() + ',(price_total - margin)'
