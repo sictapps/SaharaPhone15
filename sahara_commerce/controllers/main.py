c68a89ee3e29bcdf8953e7d0e85e04ce7cfc0058
m.salman <efrfr>
2023-05-10 16:41:14 +0300

    E-commerce 1.5

---
+++ sahara_commerce/controllers/main.py
@@ -0,0 +1,216 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo.http import request
+
+
+class WebsiteSaleInherit(WebsiteSale):
+
+    @http.route('/shop/save_shop_layout_mode', type='json', auth='public', website=True)
+    def save_shop_layout_mode(self, layout_mode):
+        assert layout_mode in ('grid', 'list'), "Invalid shop layout mode"
+        request.session['website_sale_shop_layout_mode'] = layout_mode
+
+    # ------------------------------------------------------
+    # Checkout
+    # ------------------------------------------------------
+
+    def checkout_check_address(self, order):
+        billing_fields_required = self._get_mandatory_fields_billing(order.partner_id.country_id.id)
+        if not all(order.partner_id.read(billing_fields_required)[0].values()):
+            return request.redirect('/shop/address?partner_id=%d' % order.partner_id.id)
+
+        shipping_fields_required = self._get_mandatory_fields_shipping(order.partner_shipping_id.country_id.id)
+        if not all(order.partner_shipping_id.read(shipping_fields_required)[0].values()):
+            return request.redirect('/shop/address?partner_id=%d' % order.partner_shipping_id.id)
+
+    def checkout_redirection(self, order):
+        # must have a draft sales order with lines at this point, otherwise reset
+        if not order or order.state != 'draft':
+            request.session['sale_order_id'] = None
+            request.session['sale_transaction_id'] = None
+            return request.redirect('/shop')
+
+        if order and not order.order_line:
+            return request.redirect('/shop/cart')
+
+        # if transaction pending / done: redirect to confirmation
+        tx = request.env.context.get('website_sale_transaction')
+        if tx and tx.state != 'draft':
+            return request.redirect('/shop/payment/confirmation/%s' % order.id)
+
+    def checkout_values(self, **kw):
+        order = request.website.sale_get_order(force_create=1)
+        shippings = []
+        if order.partner_id != request.website.user_id.sudo().partner_id:
+            Partner = order.partner_id.with_context(show_address=1).sudo()
+            shippings = Partner.search([
+                ("id", "child_of", order.partner_id.commercial_partner_id.ids),
+                '|', ("type", "in", ["delivery", "other"]), ("id", "=", order.partner_id.commercial_partner_id.id)
+            ], order='id desc')
+            if shippings:
+                if kw.get('partner_id') or 'use_billing' in kw:
+                    if 'use_billing' in kw:
+                        partner_id = order.partner_id.id
+                    else:
+                        partner_id = int(kw.get('partner_id'))
+                    if partner_id in shippings.mapped('id'):
+                        order.partner_shipping_id = partner_id
+
+        values = {
+            'order': order,
+            'shippings': shippings,
+            'only_services': order and order.only_services or False
+        }
+        return values
+
+    def _get_mandatory_fields_billing(self, country_id=False):
+        req = ["name", "email", "street", "city", "country_id"]
+        if country_id:
+            country = request.env['res.country'].browse(country_id)
+            if country.state_required:
+                req += ['state_id']
+            # if country.zip_required:
+            #     req += ['zip']
+        return req
+
+    def _get_mandatory_fields_shipping(self, country_id=False):
+        req = ["name", "street", "city", "country_id"]
+        if country_id:
+            country = request.env['res.country'].browse(country_id)
+            if country.state_required:
+                req += ['state_id']
+            # if country.zip_required:
+            #     req += ['zip']
+        return req
+
+    def checkout_form_validate(self, mode, all_form_values, data):
+        # mode: tuple ('new|edit', 'billing|shipping')
+        # all_form_values: all values before preprocess
+        # data: values after preprocess
+        error = dict()
+        error_message = []
+
+        # prevent name change if invoices exist
+        if data.get('partner_id'):
+            partner = request.env['res.partner'].browse(int(data['partner_id']))
+            if partner.exists() and partner.name and not partner.sudo().can_edit_vat() and 'name' in data and (data['name'] or False) != (partner.name or False):
+                error['name'] = 'error'
+                error_message.append(_('Changing your name is not allowed once invoices have been issued for your account. Please contact us directly for this operation.'))
+
+        # Required fields from form
+        required_fields = [f for f in (all_form_values.get('field_required') or '').split(',') if f]
+
+        # Required fields from mandatory field function
+        country_id = int(data.get('country_id', False))
+        required_fields += mode[1] == 'shipping' and self._get_mandatory_fields_shipping(country_id) or self._get_mandatory_fields_billing(country_id)
+
+        # error message for empty required fields
+        for field_name in required_fields:
+            if not data.get(field_name):
+                error[field_name] = 'missing'
+
+        # email validation
+        if data.get('email') and not tools.single_email_re.match(data.get('email')):
+            error["email"] = 'error'
+            error_message.append(_('Invalid Email! Please enter a valid email address.'))
+
+        # vat validation
+        Partner = request.env['res.partner']
+        if data.get("vat") and hasattr(Partner, "check_vat"):
+            if country_id:
+                data["vat"] = Partner.fix_eu_vat_number(country_id, data.get("vat"))
+            partner_dummy = Partner.new(self._get_vat_validation_fields(data))
+            try:
+                partner_dummy.check_vat()
+            except ValidationError as exception:
+                error["vat"] = 'error'
+                error_message.append(exception.args[0])
+
+        if [err for err in error.values() if err == 'missing']:
+            error_message.append(_('Some required fields are empty.'))
+
+        return error, error_message
+
+    def _get_vat_validation_fields(self, data):
+        return {
+            'vat': data['vat'],
+            'country_id': int(data['country_id']) if data.get('country_id') else False,
+        }
+
+    def _checkout_form_save(self, mode, checkout, all_values):
+        Partner = request.env['res.partner']
+        if mode[0] == 'new':
+            partner_id = Partner.sudo().with_context(tracking_disable=True).create(checkout).id
+        elif mode[0] == 'edit':
+            partner_id = int(all_values.get('partner_id', 0))
+            if partner_id:
+                # double check
+                order = request.website.sale_get_order()
+                shippings = Partner.sudo().search([("id", "child_of", order.partner_id.commercial_partner_id.ids)])
+                if partner_id not in shippings.mapped('id') and partner_id != order.partner_id.id:
+                    return Forbidden()
+                Partner.browse(partner_id).sudo().write(checkout)
+        return partner_id
+
+    def values_preprocess(self, order, mode, values):
+        new_values = dict()
+        partner_fields = request.env['res.partner']._fields
+
+        for k, v in values.items():
+            # Convert the values for many2one fields to integer since they are used as IDs
+            if k in partner_fields and partner_fields[k].type == 'many2one':
+                new_values[k] = bool(v) and int(v)
+            # Store empty fields as `False` instead of empty strings `''` for consistency with other applications like
+            # Contacts.
+            elif v == '':
+                new_values[k] = False
+            else:
+                new_values[k] = v
+
+        return new_values
+
+    def values_postprocess(self, order, mode, values, errors, error_msg):
+        new_values = {}
+        authorized_fields = request.env['ir.model']._get('res.partner')._get_form_writable_fields()
+        for k, v in values.items():
+            # don't drop empty value, it could be a field to reset
+            if k in authorized_fields and v is not None:
+                new_values[k] = v
+            else:  # DEBUG ONLY
+                if k not in ('field_required', 'partner_id', 'callback', 'submitted'): # classic case
+                    _logger.debug("website_sale postprocess: %s value has been dropped (empty or not writable)" % k)
+
+        if request.website.specific_user_account:
+            new_values['website_id'] = request.website.id
+
+        if mode[0] == 'new':
+            new_values['company_id'] = request.website.company_id.id
+            new_values['team_id'] = request.website.salesteam_id and request.website.salesteam_id.id
+            new_values['user_id'] = request.website.salesperson_id.id
+
+        lang = request.lang.code if request.lang.code in request.website.mapped('language_ids.code') else None
+        if lang:
+            new_values['lang'] = lang
+        if mode == ('edit', 'billing') and order.partner_id.type == 'contact':
+            new_values['type'] = 'other'
+        if mode[1] == 'shipping':
+            new_values['parent_id'] = order.partner_id.commercial_partner_id.id
+            new_values['type'] = 'delivery'
+
+        return new_values, errors, error_msg
+
+
+    @http.route(['/shop/country_infos/<model("res.country"):country>'], type='json', auth="public", methods=['POST'],
+                website=True)
+    def country_infos(self, country, mode, **kw):
+        return dict(
+            fields=country.get_address_fields(),
+            states=[(st.id, st.name, st.code) for st in country.get_website_sale_states(mode=mode)],
+            phone_code=country.phone_code,
+            # zip_required=country.zip_required,
+            state_required=country.state_required,
+        )
+
+
+
