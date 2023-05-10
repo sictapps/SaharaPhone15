0f0899f644e5434fd43e6ef39600fa6c28df4bb3
m.salman <efrfr>
2023-05-10 13:12:47 +0300

    E-commerce 1.2

--- sahara_commerce/__manifest__.py
+++ sahara_commerce/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'website_sale'],
+    'depends': ['base', 'sale'],
 
     # always loaded
     'data': [
