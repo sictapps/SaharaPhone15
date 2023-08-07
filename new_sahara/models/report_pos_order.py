99da90c8614bbe2976c3b473f186c048bc422019
m.salman <efrfr>
2023-08-07 13:02:43 +0300

    cost POS 1.1

--- new_sahara/models/report_pos_order.py
+++ new_sahara/models/report_pos_order.py
@@ -9,4 +9,4 @@ class PosOrderReport(models.Model):
 
     def _select(self):
         return super(PosOrderReport, self)._select() + \
-               ',(SUM(ROUND((l.qty * l.price_subtotal_incl) * (100 - l.discount) / 100 / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END, cu.decimal_places)) - SUM(l.price_subtotal - l.total_cost / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END)) AS cost_pos'
+               ',((SUM(ROUND((l.qty * l.price_unit) * (100 - l.discount) / 100 / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END, cu.decimal_places)) - SUM(l.price_subtotal - l.total_cost / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END))+ SUM(l.price_subtotal_incl - l.price_subtotal)) AS cost_pos'
