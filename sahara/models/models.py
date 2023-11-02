bdda2c702f0b99885fe2677a87167f7209a6e1c6
m.salman <efrfr>
2023-11-02 16:18:51 +0300

    new_Tax

--- sahara/models/models.py
+++ sahara/models/models.py
@@ -5,6 +5,21 @@ import pytz
 from odoo import tools
 
 
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+    _description = "Account Move Line"
+
+    tax_amount = fields.Float(string="Tax Amount", compute="_compute_tax_amount")
+
+    @api.onchange("quantity", "tax_ids")
+    def _compute_tax_amount(self):
+        for move_line_id in self:
+            tax_only = 0
+            for tax_id in move_line_id.tax_ids:
+                tax_only += move_line_id.price_unit * (tax_id.amount / 100)
+            move_line_id.tax_amount = tax_only * move_line_id.quantity
+
+
 class TextAccountMove(models.Model):
     _inherit = "account.move"
 
