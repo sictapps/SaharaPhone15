a7742249cc150b8361c6407a32635fdac65cbc41
saharadev.2022@gmail.com <!Admin123123>
2024-01-10 09:51:04 +0300

    tabby01

--- sahara_invoice_tabby/__manifest__.py
+++ sahara_invoice_tabby/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sahara', 'sahara_planet'],
+    'depends': ['base', 'sahara'],
 
     # always loaded
     'data': [
